a0bd420cc71d96277e1c5c444ec60ffdf01de882
Byron Williams <byronawilliams@gmail.com>
2025-04-09 19:52:59 -0700

    Remove legacy and duplicate workflow files

--- mirrors-isort/setup.py
+++ mirrors-isort/setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup
 
-
 setup(
-    name='pre_commit_placeholder_package',
-    version='0.0.0',
-    install_requires=['isort==5.10.1'],
+    name="pre_commit_placeholder_package",
+    version="0.0.0",
+    install_requires=["isort==5.10.1"],
 )
